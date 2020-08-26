7d7f840ca0322b250e77d5f37d7d048c02ca7818
shahriyarmammadli <shahriyarmammadli@outlook.com>
2020-08-26 21:17:23 +0400

    "Testing commiting from pycharm"

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ def rotate(nums, k):
     nums[:] = [] if len(nums) == 0 else nums[len(nums) - k%len(nums):] + nums[:len(nums)-k%len(nums)]
     return nums
 
-print(rotate([1,2,3,4,5,6,7], 3))
+print(rotate([1,2], 3))
